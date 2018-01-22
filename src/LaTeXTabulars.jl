f8ce112ce17cc7901854ce05f502f35766755881
Tamas K. Papp <tkpapp@gmail.com>
2018-01-22 20:39:58 +0100

    Initial commit.

--- src/LaTeXTabulars.jl
+++ src/LaTeXTabulars.jl
@@ -1,5 +1,70 @@
 module LaTeXTabulars
 
-# package code goes here
+using DocStringExtensions
+
+export Rule, print_tabular
+
+
+# cells
+
+"""
+    $SIGNATURES
+"""
+function print_cell(io::IO, ::T) where T
+    info("Define a method for writing $T objects to LaTeX.")
+    throw(MethodError(print_tex, Tuple{IO, T}))
+end
+
+print_cell(io::IO, x::Real) = print(io, string(x))
+
+print_cell(io::IO, s::AbstractString) = print(io, s)
+
+
+# lines and line-like objects
+
+struct Rule{T} end
+
+Rule(kind::Symbol) = Rule{kind}()
+
+print_line(io::IO, ::Rule{:top}) = println(io, "\\toprule")
+
+print_line(io::IO, ::Rule{:mid}) = println(io, "\\midrule")
+
+print_line(io::IO, ::Rule{:bottom}) = println(io, "\\bottomrule")
+
+function print_line(io::IO, cells)
+    for (column, cell) in enumerate(cells)
+        column == 1 || print(io, " & ")
+        print_cell(io, cell)
+    end
+    println(io, " \\\\")
+end
+
+
+# tabular
+
+"""
+    $SIGNATURES
+
+Print `lines` to `io` as a LaTeX `tabular` environment.
+
+`cols` specifies the columns, and follows the syntax of LaTeX.
+
+Each element in `lines` is an iterable of cells (not checked for length
+consistency), or a separator like [`Rule`](@ref).
+"""
+function print_tabular(io::IO, cols::AbstractString, lines)
+    println(io, "\\begin{tabular}{$cols}")
+    for line in lines
+        print_line(io, line)
+    end
+    println(io, "\\end{tabular}")
+end
+
+function print_tabular(::Type{String}, args...)
+    io = IOBuffer()
+    print_tabular(io, args...)
+    String(take!(io))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,25 @@
 using LaTeXTabulars
 using Base.Test
+using LaTeXStrings
 
-# write your own tests here
-@test 1 == 2
+squash_whitespace(string) = strip(replace(string, r"[ \n\t]+", " "))
+
+@test squash_whitespace(" something  \n with line  breaks \n  and   stuff \n") ==
+    "something with line breaks and stuff"
+
+≅(a, b) = squash_whitespace(a) == squash_whitespace(b)
+
+@test print_tabular(String, "ll", [Rule(:top),
+                                   [L"\alpha", L"\beta", "sum"],
+                                   Rule(:mid),
+                                   [1, 2, 3],
+                                   [4.0, "5", "six"],
+                                   Rule(:bottom)]) ≅
+                                       raw"\begin{tabular}{ll}
+                                        \toprule
+                                        $\alpha$ & $\beta$ & sum \\
+                                        \midrule
+                                        1 & 2 & 3 \\
+                                        4.0 & 5 & six \\
+                                        \bottomrule
+                                        \end{tabular}"
