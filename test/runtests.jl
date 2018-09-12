7c8d0f74eef38f528ac56e90f54149590755aea5
Jeroen van Zundert <jvanzundert@bluecove.com>
2018-09-12 20:19:58 +0100

    Update for Julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,11 @@ using LaTeXTabulars
 # for testing
 using LaTeXTabulars: latex_cell
 
-using Base.Test
+using Test
 using LaTeXStrings
 
 "Normalize whitespace, for more convenient testing."
-squash_whitespace(string) = strip(replace(string, r"[ \n\t]+", " "))
+squash_whitespace(string) = strip(replace(string, r"[ \n\t]+" => " "))
 
 @test squash_whitespace(" something  \n with line  breaks \n  and   stuff \n") ==
     "something with line breaks and stuff"
@@ -40,13 +40,15 @@ squash_whitespace(string) = strip(replace(string, r"[ \n\t]+", " "))
                  \bottomrule
                  \end{tabular}"
 
+    tlatex = replace(tlatex, "\r\n"=>"\n")
     @test latex_tabular(String, tb, tlines) ≅ tlatex
     tmp = tempname()
     latex_tabular(tmp, tb, tlines)
     @test isfile(tmp) && readstring(tmp) ≅ tlatex
+    @test read(tmp, String) ≅ tlatex
 end
 
-@test_throws ArgumentError latex_cell(STDOUT, MultiColumn(2, :BAD, ""))
+@test_throws ArgumentError latex_cell(stdout, MultiColumn(2, :BAD, ""))
 @test_throws ArgumentError CMidRule(3, 1)     # not ≤
-@test_throws MethodError latex_cell(STDOUT, ("un", "supported"))
+@test_throws MethodError latex_cell(stdout, ("un", "supported"))
 @test_throws MethodError CMidRule(1, 1, 1, 2) # invalid types
